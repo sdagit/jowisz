73d89b80304687cfafdc6dda1b06c308e396a9ee
Karina Wilk desktop <sda_git@protonmail.com>
2018-10-10 16:21:14 +0200

    Szkielet aplikacji

---
+++ src/com/sdacademy/Main.java
@@ -0,0 +1,22 @@
+package com.sdacademy;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Witaj w asystencie pisarza! Wpisz dowolne zdanie!");
+
+        //TODO: Odbierz zdanie od użytkownika i przechowaj je w zmiennej.
+
+        System.out.println("Teraz podaj jaką operację mam wykonać: \r\n" +
+                "1 - policzyć słowa w zdaniu, które podałeś\n" +
+                "2 - policzyć wszystkie litery w zdaniu\n" +
+                "3 - policzyć białe znaki w zdaniu\n");
+
+
+        //TODO Odczytaj liczbę wprowadzoną przez użytkownika
+        //TODO: jeśli wprowadzona liczba jest inna niż 1,2 lub 3 - zakończ aplikację.
+        //TODO: Jeśli liczba jest równa 1, 2, lub 3 - wywołaj konkretną metodę (którą członkowie zespołu
+        // musisz wcześniej napisać).
+    }
+}
